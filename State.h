a8bd0ff244a4cbab6644ce7a6c6b6fd419622df7
hoangteo0103 <51512556+hoangteo0103@users.noreply.github.com>
2022-11-15 21:53:51 +0700

    Implement function of Class State

--- State.h
+++ State.h
@@ -1,5 +1,37 @@
-#pragma once
+#ifndef STATE_H
+#define STATE_H
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
+#include <time.h>
+#include<iostream>
+#include<stack>
+
+using namespace sf;
+using namespace std;
 class State
 {
-};
+protected:
+    stack<State*>* states;
+    RenderWindow* app;
+    bool paused;
+    bool quit;
+    Vector2i mousePosWindow;
+    Vector2f mousePosView;
 
+public:
+    State(RenderWindow* app, stack<State*>* states);
+    virtual ~State();
+    // Functions
+    void pauseState();
+    void unpauseState();
+    const bool& getQuit() const;
+    virtual void checkForQuit();
+    virtual void updateKeyBinds() = 0;
+    virtual void updateMousePositions();
+    virtual void endState() = 0;
+    virtual void update() = 0;
+    virtual void render(RenderTarget* target = nullptr) = 0;
+
+};
+#endif
