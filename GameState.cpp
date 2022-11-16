c5c0c209dadff454c4062bbdaaed1cb1b51efc34
hoangteo0103 <51512556+hoangteo0103@users.noreply.github.com>
2022-11-16 09:31:14 +0700

    Set up basic form of GameState

--- GameState.cpp
+++ GameState.cpp
@@ -1,1 +1,161 @@
 #include "GameState.h"
+
+void GameState::Reset()
+{
+    this->ok = false;
+    this->isUpdated = false;
+    this->clock.Reset();
+    this->paused = false;
+    this->previousTime = 0;
+}
+
+void GameState::initFonts()
+{
+    if (!this->font.loadFromFile("font/Contb.ttf"))
+    {
+
+    }
+    //texture.loadFromFile("images/game1.png");
+    this->background.setTexture(texture);
+}
+GameState::GameState(RenderWindow* app, stack<State*>* states, int sizeX, int sizeY, int numberOfBombs, bool saved)
+    :State(app, states)
+{
+    // Init
+    this->ok = false;
+    this->isUpdated = false;
+    this->clock.Start();
+    this->initFonts();
+}
+
+GameState ::~GameState()
+{
+    //delete this->pmenu;
+    auto it = this->buttons.begin();
+    for (it = this->buttons.begin(); it != this->buttons.end(); ++it)
+    {
+        delete it->second;
+    }
+}
+
+
+
+void GameState::updateKeyBinds()
+{
+    this->checkForQuit();
+}
+
+void GameState::endState()
+{
+    this->quit = true;
+    cout << "End MainMenu" << endl;
+}
+
+void GameState::updatePaused()
+{
+    //this->pmenu.updateMousePositions(mousePosView);
+}
+
+bool cmp(pair<int, int > a, pair<int, int > b)
+{
+    if (a.first != b.first)
+        return a.first > b.first;
+    return a.second < b.second;
+}
+void GameState::updateBeginner(int time_now)
+{
+    ifstream ifs("Leaderboard/leaderboardBeginner.ini");
+    vector< int > tmp;
+    tmp.push_back(time_now);
+    int time;
+    while (ifs >> time)
+    {
+        tmp.push_back(time);
+    }
+    sort(tmp.begin(), tmp.end());
+    ofstream ofs("Leaderboard/leaderboardBeginner.ini");
+    for (int i = 0; i < min(10, (int)tmp.size()); i++)
+    {
+        ofs << tmp[i] << endl;
+    }
+}
+void GameState::updateIntermediate(int time_now)
+{
+    ifstream ifs("Leaderboard/leaderboardIntermediate.ini");
+    vector< int > tmp;
+    tmp.push_back(time_now);
+    int time;
+    while (ifs >> time)
+    {
+        tmp.push_back(time);
+    }
+    sort(tmp.begin(), tmp.end());
+    ofstream ofs("Leaderboard/leaderboardIntermediate.ini");
+    for (int i = 0; i < min(10, (int)tmp.size()); i++)
+    {
+        ofs << tmp[i] << endl;
+    }
+}
+
+void GameState::updateExpert(int time_now)
+{
+    ifstream ifs("Leaderboard/leaderboardExpert.ini");
+    vector< int > tmp;
+    tmp.push_back(time_now);
+    int time;
+    while (ifs >> time)
+    {
+        tmp.push_back(time);
+    }
+    sort(tmp.begin(), tmp.end());
+    ofstream ofs("Leaderboard/leaderboardExpert.ini");
+    for (int i = 0; i < min(10, (int)tmp.size()); i++)
+    {
+        ofs << tmp[i] << endl;
+    }
+}
+
+void GameState::updateLeaderBoard()
+{
+   
+}
+
+
+void GameState::updateWinState()
+{
+    
+}
+
+void GameState::updateLoseState()
+{
+   
+}
+
+void GameState::updateButtons()
+{
+    for (auto& it : this->buttons)
+    {
+        it.second->update(this->mousePosView);
+    }
+}
+
+void GameState::update()
+{
+
+    this->updateMousePositions();
+    this->updateKeyBinds();
+  
+}
+
+void GameState::renderButtons(RenderTarget* target)
+{
+    for (auto& it : this->buttons)
+    {
+        it.second->render(target);
+    }
+}
+
+void GameState::render(RenderTarget* target)
+{
+}
+
--- GameState.h
+++ GameState.h
@@ -1,6 +1,97 @@
-#pragma once
-#include "Base.h"
-class GameState
+#ifndef GameState_H
+#define GameState_H
+#include "State.h"
+#include "Button.h"
+#include "PauseState.h"
+#include "WinState.h"
+#include "LoseState.h"
+#include "PlayAgainState.h"
+namespace st {
+    struct Timer {
+        sf::Clock mC;
+        float runTime;
+        bool bPaused;
+
+        Timer() {
+            bPaused = false;
+            runTime = 0;
+            mC.restart();
+        }
+
+        void Reset() {
+            mC.restart();
+            runTime = 0;
+            bPaused = false;
+        }
+
+        void Start() {
+            if (bPaused) {
+                mC.restart();
+            }
+            bPaused = false;
+        }
+
+        void Pause() {
+            if (!bPaused) {
+                runTime += mC.getElapsedTime().asSeconds();
+            }
+            bPaused = true;
+        }
+
+        float GetElapsedSeconds() {
+            if (!bPaused) {
+                return runTime + mC.getElapsedTime().asSeconds();
+            }
+            return runTime;
+        }
+    };
+}
+
+class GameState :
+    public State
 {
+private:
+    //PauseMenu pmenu;
+    //WinState winState;
+    LoseState loseState;
+    PlayAgainState againState;
+    st::Timer clock;
+    Time t;
+    Text lblTime;
+    //ostringstream ssTime;
+    map<string, Button* > buttons;
+    Font font;
+    Texture texture;
+    Sprite background;
+    bool isUpdated;
+    bool ok;
+    int previousTime;
+    int sizeX, sizeY;
+    int numberOfBombs;
+    void initFonts();
+    void initButtons();
+
+public:
+    GameState(RenderWindow* app, stack<State*>* states, int sizeX, int sizeY, int numberOfBombs, bool saved);
+    virtual ~GameState();
+    // Functions
+    void Reset();
+    void updatePaused();
+    void updateKeyBinds();
+    void updateBeginner(int time_now);
+    void updateIntermediate(int time_now);
+    void updateExpert(int time_now);
+    void updateLeaderBoard();
+    void updateButtons();
+    void updateWinState();
+    void updateLoseState();
+    const bool& getLose() const;
+    const bool& getWin() const;
+    void endState();
+    void update();
+    void render(RenderTarget* target = NULL);
+    void renderButtons(RenderTarget* target = NULL);
+
 };
+#endif
 
