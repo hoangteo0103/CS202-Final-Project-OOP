720d914c2b5ffee4a29426ebb6f9a82833cbb6b6
phucst123 <huuphucst123@gmail.com>
2022-11-22 00:42:51 +0700

    add animation, move, update

--- CPEOPLE.cpp
+++ CPEOPLE.cpp
@@ -1,6 +1,8 @@
 #include "CPEOPLE.h"
 
-CPEOPLE::CPEOPLE(sf::Texture* texture, sf::Vector2u image_contain, float switch_time) {
+CPEOPLE::Animation::Animation() {}
+
+CPEOPLE::Animation::Animation(sf::Texture* texture, sf::Vector2u image_contain, float switch_time) {
 	this->image_contain = image_contain;
 	this->switch_time = switch_time;
 	total_time = 0.f;
@@ -10,7 +12,7 @@ CPEOPLE::CPEOPLE(sf::Texture* texture, sf::Vector2u image_contain, float switch_
 	uv_rect.height = texture->getSize().y / float(image_contain.y);
 }
 
-void CPEOPLE::update(int row, float delta_time) {
+void CPEOPLE::Animation::update(int row, float delta_time /*int direction*/) {
 	current_image.y = row;
 	total_time += delta_time;
 	if (total_time >= switch_time) {
@@ -24,4 +26,62 @@ void CPEOPLE::update(int row, float delta_time) {
 
 	uv_rect.left = current_image.x * uv_rect.width;
 	uv_rect.top = current_image.y * uv_rect.height;
+}
+
+
+void CPEOPLE::Mouvment::goDirection(const int& dir, sf::Sprite& spritesheet, float speed, float delta_time)
+{
+	float dis = speed * delta_time;
+	switch (dir) {
+	case 0:
+		spritesheet.move(sf::Vector2f(0, -dis));
+		break;
+	case 1:
+		spritesheet.move(sf::Vector2f(0, dis));
+		break;
+	case 2:
+		spritesheet.move(sf::Vector2f(dis, 0));
+		break;
+	case 3:
+		spritesheet.move(sf::Vector2f(-dis, 0));
+		break;
+	}
+}
+
+
+CPEOPLE::CPEOPLE(sf::Texture* texture, sf::Vector2u image_contain, float switch_time, float speed) {
+	this->animation = Animation(texture, image_contain, switch_time);
+	this->speed = speed;
+	row = 0;
+	face_direction = 0;
+
+	body.setTexture(*texture);
+}
+
+
+void CPEOPLE::draw(sf::RenderWindow& window) {
+	window.draw(body);
+}
+
+void CPEOPLE::move(float delta_time) {
+	Mouvment mouvment;
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+		mouvment.goDirection(2, body, speed, delta_time);
+		row = 2;
+	}
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+		mouvment.goDirection(3, body, speed, delta_time);
+		row = 1;
+	}
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+		mouvment.goDirection(0, body, speed, delta_time);
+		row = 3;
+	}
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+		mouvment.goDirection(1, body, speed, delta_time);
+		row = 0;
+	}
+
+	this->animation.update(row, delta_time);
+	this->body.setTextureRect(this->animation.uv_rect);
 }
--- CPEOPLE.h
+++ CPEOPLE.h
@@ -1,21 +1,46 @@
 #pragma once
 #include "Base.h"
+
+
 class CPEOPLE : public Object
 {
-private:
-	float total_time, switch_time;
-	sf::Vector2u image_contain;
-	sf::Vector2u current_image;
+public:
+	
+	class Animation {
+	private:
+		float total_time, switch_time;
+		sf::Vector2u image_contain;
+		sf::Vector2u current_image;
 
-	void mode();
-
+	public:
+		void update(int row, float delta_time /*int direction*/);
+		sf::IntRect uv_rect;
+		Animation(sf::Texture* texture, sf::Vector2u image_contain, float switch_time);
+		Animation();
+		
+	};
 	
 
-public:
-	sf::IntRect uv_rect;
+    class Mouvment {
+    public:
+        Mouvment() {};
+		void goDirection(const int& dir, sf::Sprite& spritesheet, float speed, float delta_time);
+    };
 
-	CPEOPLE(sf::Texture* texture, sf::Vector2u image_contain, float switch_time);
 
-	void update(int row, float delta_time);
+	CPEOPLE(sf::Texture* texture, sf::Vector2u image_contain, float switch_time, float speed);
+
+	//void mode();
+	void move(float delta_time);
+	//void update(float delta_time);
+	void draw(sf::RenderWindow& window);
+
+private:
+	
+	sf::Sprite body;
+	Animation animation;
+	int row;
+	float speed;
+	int face_direction; // 0 - down, 1 - left, 2 - right, 3 - up
 };
 
