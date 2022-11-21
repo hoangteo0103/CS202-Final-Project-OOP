5c06b4caf774db93389404783a15c2c7226786f3
phucst123 <huuphucst123@gmail.com>
2022-11-21 22:24:15 +0700

    add update, constructor of CPEOPLE

--- CPEOPLE.cpp
+++ CPEOPLE.cpp
@@ -1,1 +1,27 @@
 #include "CPEOPLE.h"
+
+CPEOPLE::CPEOPLE(sf::Texture* texture, sf::Vector2u image_contain, float switch_time) {
+	this->image_contain = image_contain;
+	this->switch_time = switch_time;
+	total_time = 0.f;
+	current_image.x = 0;
+
+	uv_rect.width = texture->getSize().x / float(image_contain.x);
+	uv_rect.height = texture->getSize().y / float(image_contain.y);
+}
+
+void CPEOPLE::update(int row, float delta_time) {
+	current_image.y = row;
+	total_time += delta_time;
+	if (total_time >= switch_time) {
+		total_time -= switch_time;
+		++current_image.x;
+
+		if (current_image.x >= image_contain.x) {
+			current_image.x = 0;
+		}
+	}
+
+	uv_rect.left = current_image.x * uv_rect.width;
+	uv_rect.top = current_image.y * uv_rect.height;
+}
--- CPEOPLE.h
+++ CPEOPLE.h
@@ -3,6 +3,19 @@
 class CPEOPLE : public Object
 {
 private:
+	float total_time, switch_time;
+	sf::Vector2u image_contain;
+	sf::Vector2u current_image;
+
 	void mode();
+
+	
+
+public:
+	sf::IntRect uv_rect;
+
+	CPEOPLE(sf::Texture* texture, sf::Vector2u image_contain, float switch_time);
+
+	void update(int row, float delta_time);
 };
 
