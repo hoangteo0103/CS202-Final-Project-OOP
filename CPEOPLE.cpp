1d7904c38800daef090d558e8cfe85cbebc64716
phucst123 <huuphucst123@gmail.com>
2022-12-02 00:23:17 +0700

    add boundary condition
    boundary depends on size of map texture

--- CPEOPLE.cpp
+++ CPEOPLE.cpp
@@ -8,21 +8,30 @@ CPEOPLE::CPEOPLE(string path,  sf::Vector2u image_contain, float switch_time, fl
 
 void CPEOPLE::move(float delta_time) {
 	Mouvment mouvment;
+	int temp_size = 740;
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
-		mouvment.goDirection(2, sprite, speed, delta_time);
-		row = 2;
+		if (this->getPosition().x < temp_size) {
+			mouvment.goDirection(2, sprite, speed, delta_time);
+			row = 2;
+		}
 	}
 	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
-		mouvment.goDirection(3, sprite, speed, delta_time);
-		row = 1;
+		if (this->getPosition().x > 0) {
+			mouvment.goDirection(3, sprite, speed, delta_time);
+			row = 1;
+		}
 	}
 	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
-		mouvment.goDirection(0, sprite, speed, delta_time);
-		row = 3;
+		if (this->getPosition().y > 0) {
+			mouvment.goDirection(0, sprite, speed, delta_time);
+			row = 3;
+		}
 	}
 	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
-		mouvment.goDirection(1, sprite, speed, delta_time);
-		row = 0;
+		if (this->getPosition().y < temp_size) {
+			mouvment.goDirection(1, sprite, speed, delta_time);
+			row = 0;
+		}
 	}
 	//cout << sprite.getPosition().x << ' ' << sprite.getPosition().y << endl; 
 	this->animation.update(row, delta_time);
