0b78f62a5347148bb6539ba1d958d061ce0a4dee
phucst123 <huuphucst123@gmail.com>
2022-12-03 21:54:48 +0700

    add Map class

---
+++ CMap.cpp
@@ -0,0 +1,22 @@
+#include "CMap.h"
+
+
+using namespace std;
+
+CMap::CMap(const string& dir) {
+	if (!texture.loadFromFile(dir)) {
+		cout << "Could Not Load Map" << endl;
+	}
+
+	sprite.setTexture(texture);
+	size = texture.getSize();
+}
+
+
+void CMap::draw(sf::RenderWindow& window) {
+	window.draw(sprite);
+}
+
+sf::Vector2u CMap::getSize() const{
+	return size;
+}
---
+++ CMap.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "Base.h"
+
+class CMap
+{
+private:
+	sf::Vector2u size;
+	sf::Texture texture;
+	sf::Sprite sprite;
+	
+
+public:
+	CMap(const std::string& dir);
+
+	void draw(sf::RenderWindow& window);
+	sf::Vector2u getSize() const;
+};
+
