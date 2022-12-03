9c49d38d2218db206f9c83e3f6424503309891cf
phucst123 <huuphucst123@gmail.com>
2022-12-03 21:56:23 +0700

    add conditions depend on map size

--- CView.cpp
+++ CView.cpp
@@ -1,7 +1,8 @@
 #include "CView.h"
 
-CView::CView(const sf::Vector2f& window_size) {
+CView::CView(const sf::Vector2u& window_size, const sf::Vector2u& map_size) {
 	this->window_size = window_size;
+	this->map_size = map_size;
 	_view.reset(sf::FloatRect(0, 0, this->window_size.x, this->window_size.y));
 	_view.setViewport(sf::FloatRect(0, 0, 1.0f, 1.0f));
 	this->position.x = this->window_size.x / 2;
@@ -9,10 +10,10 @@ CView::CView(const sf::Vector2f& window_size) {
 }
 
 void CView::update(sf::RenderWindow& window, Object& player) {
-	int temp_size = 740;
-	if (player.getPosition().x > window_size.x / 2 && player.getPosition().x < temp_size - window_size.x/2)
+
+	if (player.getPosition().x > window_size.x / 2 && player.getPosition().x < map_size.x - window_size.x/2)
 		position.x = player.getPosition().x;
-	if (player.getPosition().y > window_size.y / 2 && player.getPosition().y < temp_size - window_size.y / 2)
+	if (player.getPosition().y > window_size.y / 2 && player.getPosition().y < map_size.y - window_size.y / 2)
 		position.y = player.getPosition().y;
 	_view.setCenter(position);
 	window.setView(_view);
--- CView.h
+++ CView.h
@@ -6,10 +6,10 @@ class CView
 {
 private:
 	sf::View _view;
-	sf::Vector2f window_size;
+	sf::Vector2u window_size, map_size;
 	sf::Vector2f position;
 public:
-	CView(const sf::Vector2f& window_size);
+	CView(const sf::Vector2u& window_size, const sf::Vector2u& map_size);
 	void update(sf::RenderWindow& window, Object& player);
 	
 };
