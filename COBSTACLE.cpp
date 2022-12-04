9c12d444b37b62ad11d096efbec12c599bc46d1a
Cohvtk <94277200+Cohvtk@users.noreply.github.com>
2022-12-04 20:53:11 +0700

    CObstacle functions

--- COBSTACLE.cpp
+++ COBSTACLE.cpp
@@ -7,14 +7,32 @@ COBSTACLE::COBSTACLE(string path, sf::Vector2u image_contain, float switch_time,
 }
 		
 
-void COBSTACLE::move(float delta_time)
+void COBSTACLE::move(float delta_time, const sf::Vector2u map_size)
 {
 	Mouvment mouvment;
 	// 1 Left 2 Right 
-	mouvment.goDirection(4 - direction, sprite, speed, delta_time);
+	mouvment.goDirection(direction, sprite, speed, delta_time);
 	//cout << sprite.getPosition().x << ' ' << sprite.getPosition().y << endl; 
 	this->animation.update(0 , delta_time);
 	this->sprite.setTextureRect(this->animation.uv_rect);
-	if(direction == 1)
-	this->sprite.setScale(-1.f, 1.f);
+	if (sprite.getPosition().x > map_size.x)
+	{
+		sprite.setPosition(sf::Vector2f(0.0f-animation.uv_rect.width, sprite.getPosition().y));
+	}
+	else if (sprite.getPosition().x < 0.0f - animation.uv_rect.width)
+	{
+		sprite.setPosition(sf::Vector2f(map_size.x, sprite.getPosition().y));
+	}
+}
+
+CVehicle::CVehicle(string path, sf::Vector2u image_contain, float switch_time, float speed, Vector2f pos, int dir) :COBSTACLE(path, image_contain, switch_time, speed, pos, dir)
+{
+}
+
+CAnimal::CAnimal(string path, sf::Vector2u image_contain, float switch_time, float speed, Vector2f pos, int dir) : COBSTACLE(path, image_contain, switch_time, speed, pos, dir)
+{
+}
+
+void CAnimal::tell()
+{
 }
--- COBSTACLE.h
+++ COBSTACLE.h
@@ -6,6 +6,20 @@ private:
 	int direction; 
 public :
 	COBSTACLE(string path, sf::Vector2u image_contain, float switch_time, float speed, Vector2f pos, int dir);
-	void move(float delta_time);
+	void move(float delta_time, const sf::Vector2u map_size);
 };
 
+class CVehicle : public COBSTACLE
+{
+private:
+public:
+	CVehicle(string path, sf::Vector2u image_contain, float switch_time, float speed, Vector2f pos, int dir);
+};
+	
+class CAnimal : public COBSTACLE
+{
+private:
+public:
+	CAnimal(string path, sf::Vector2u image_contain, float switch_time, float speed, Vector2f pos, int dir);
+	void tell(); //?
+};
