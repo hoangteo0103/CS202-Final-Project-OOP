06f36b035232b0181cd84efdd64f5c53b20b8767
phucst123 <huuphucst123@gmail.com>
2022-12-13 17:56:56 +0700

    add level (number of Lanes), add init and update

--- LanePack.cpp
+++ LanePack.cpp
@@ -3,38 +3,45 @@
 
 using namespace std;
 
-void LanePack::init(int level)
+LanePack::~LanePack() {
+	for (auto lane : lanes)
+		delete lane;
+}
+
+void LanePack::init(int level, sf::Vector2u map_size, int win_line_y)
 {
 	int numberLane;
 	switch (level)
 	{
-	case 1:
+	case 1: case 2: case 3:
 		numberLane = 5;
 		break;
-	case 2:
+	case 4: case 5:
 		numberLane = 6;
-		break;
-	case 3:
-		numberLane = 7;
-		break;
-	case 4:
-		numberLane = 8;
-		break;
-	case 5:
-		numberLane = 9;
 		break;
 	default:
 		cout << "INVALID LEVEL" << endl;
-		break;
+		exit(0);
 	}
 
-
+	int factor = map_size.y / (numberLane + 4);
+	for (int i = 0; i < numberLane; ++i) {
+		int y_position = factor * i + win_line_y;
+		Lane *new_lane = new Lane(0, 2, 3, 10.f, "clayroad.png", sf::Vector2f(0, y_position));
+		lanes.push_back(new_lane);
+	}
 
 }
 
 void LanePack::update(float delta_time)
 {
+	for (auto lane : lanes)
+		lane->update(delta_time);
+}
 
+void LanePack::draw(sf::RenderWindow& window) {
+	for (auto lane : lanes)
+		lane->draw(window);
 }
 
 void LanePack::reset()
--- LanePack.h
+++ LanePack.h
@@ -2,15 +2,21 @@
 #include "Lane.h"
 #include "Base.h"
 
+const vector<sf::Vector2f> block = { sf::Vector2f(100,700),sf::Vector2f(300,700), sf::Vector2f(500,700), sf::Vector2f(700,700), sf::Vector2f(900,700), sf::Vector2f(1100,700) };
 
 class LanePack
 {
 private:
-	vector<Lane> lanes;
-	vector<sf::Vector2u> position;
-public :
-	void init(int level);
-	void update(float delta_time); 
+	vector<Lane*> lanes;
+	vector<sf::Vector2f> position;
+public:
+
+	~LanePack();
+	void init(int level, sf::Vector2u map_size, int win_line_y);
+	void update(float delta_time);
+	void draw(sf::RenderWindow& window);
 	void reset();
+
+
 };
 
