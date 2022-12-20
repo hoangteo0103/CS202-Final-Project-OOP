d36d16c378b699cea970f8332da497adf68e4f41
hoangteo0103 <51512556+hoangteo0103@users.noreply.github.com>
2022-12-21 05:08:38 +0700

    Fix the wrong position of Obstacle between Lane

--- LanePack.cpp
+++ LanePack.cpp
@@ -37,8 +37,8 @@ void LanePack::init(int speed , int level, sf::Vector2u map_size, int win_line_y
 	cout << "number of lane: " << numberLane << endl;
 
 	for (int i = 0; i < numberLane; ++i) {
-		cout << "i: " << i << endl;
 		int y_position = (distance_factor+ROADHEIGHT) * i + win_line_y + offset;
+		cout << y_position << endl; 
 		posObstacles.push_back(y_position);
 		//cout << "y_position: " << y_position << endl;
 		uniform_int_distribution<int> range(0, 1);
@@ -53,12 +53,12 @@ void LanePack::generateObstacle()
 {
 	random_device rd;
 	mt19937 rng(rd());
-	for (int i = 0; i < numberLanes; i++)
+	for (int i = 1; i < numberLanes; i++)
 	{
 		uniform_int_distribution<int> range(0, OBSTACLETEXTUREPATH.size() - 1);
 		int type = range(rng); 
 		uniform_int_distribution <int > range2(0, 2880 - SIZEOBSTACLE[type]);
-		Vector2f pos = Vector2f(posObstacles[i] - ROADHEIGHT , range2(rng));
+		Vector2f pos = Vector2f(range2(rng) , posObstacles[i] + ROADHEIGHT);
 		COBSTACLE* now = new COBSTACLE(OBSTACLETEXTUREPATH[type], Vector2u(1, 1), 0, 0, pos, 0); 
 		obstacles.push_back(now);
 	}
