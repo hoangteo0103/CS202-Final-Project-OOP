6a724a2697c2f8138d6c9f6af632dd4921611571
hoangteo0103 <51512556+hoangteo0103@users.noreply.github.com>
2022-12-20 16:21:59 +0700

    Add Obstacle between lane ( Position and Scale issue)

--- LanePack.cpp
+++ LanePack.cpp
@@ -29,6 +29,7 @@ void LanePack::init(int speed , int level, sf::Vector2u map_size, int win_line_y
 	}
 	this->speed = speed;
 	int offset = 50;
+	this->numberLanes = numberLane;
 
 	random_device rd;
 	mt19937 rng(rd());
@@ -38,14 +39,32 @@ void LanePack::init(int speed , int level, sf::Vector2u map_size, int win_line_y
 	for (int i = 0; i < numberLane; ++i) {
 		cout << "i: " << i << endl;
 		int y_position = (distance_factor+ROADHEIGHT) * i + win_line_y + offset;
+		posObstacles.push_back(y_position + ROADHEIGHT);
 		//cout << "y_position: " << y_position << endl;
 		uniform_int_distribution<int> range(0, 1);
 		int type = range(rng);
 		Lane *new_lane = new Lane(0, 2, 3, speed, type, sf::Vector2f(0, y_position));
 		lanes.push_back(new_lane);
 	}
-
+	this->generateObstacle();
 }
+
+void LanePack::generateObstacle()
+{
+	random_device rd;
+	mt19937 rng(rd());
+	for (int i = 0; i < numberLanes; i++)
+	{
+		uniform_int_distribution<int> range(0, OBSTACLETEXTUREPATH.size() - 1);
+		int type = range(rng); 
+		uniform_int_distribution <int > range2(0, 2880 - SIZEOBSTACLE[type]);
+		Vector2f pos = Vector2f(posObstacles[i] + ROADHEIGHT , range2(rng));
+		COBSTACLE* now = new COBSTACLE(OBSTACLETEXTUREPATH[type], Vector2u(1, 1), 0, 0, pos, 0); 
+		obstacles.push_back(now);
+	}
+}
+
+
 
 void LanePack::update(float delta_time)
 {
@@ -60,13 +79,16 @@ void LanePack::update(float delta_time)
 void LanePack::draw(sf::RenderWindow& window) {
 	for (auto lane : lanes)
 		lane->draw(window);
+	for (auto obstacle : obstacles)
+		cout << 1 , obstacle->draw(window);
 }
 
 void LanePack::reset(int speed , int level, sf::Vector2u map_size, int win_line_y)
 {
 	for (auto lane : lanes)
 		delete lane;
-
+	obstacles.clear();
+	posObstacles.clear();
 	lanes.clear();
 	this->init(speed , level, map_size, win_line_y);
 }
--- LanePack.h
+++ LanePack.h
@@ -3,12 +3,18 @@
 #include "Base.h"
 #include <random>
 
+const vector<string> OBSTACLETEXTUREPATH = { "asset\\obstacle\\obstacle0.png" }; // "asset\\road\\road1.png", "asset\\road\\road2.png", "asset\\road\\road3.png" };
+const vector<int> SIZEOBSTACLE = { 59 }; // "asset\\road\\road1.png", "asset\\road\\road2.png", "asset\\road\\road3.png" };
+
 
 class LanePack
 {
 private:
 	float speed; 
+	int numberLanes; 
 	vector<Lane*> lanes;
+	vector<COBSTACLE*> obstacles; 
+	vector<int> posObstacles; 
 	vector<sf::Vector2f> position;
 	int distance_factor;
 public:
@@ -18,6 +24,7 @@ public:
 	void update(float delta_time);
 	void draw(sf::RenderWindow& window);
 	void reset(int speed ,int level, sf::Vector2u map_size, int win_line_y);
+	void generateObstacle();
 	int getNumOfLanes();
 	friend class CPEOPLE;
 };
