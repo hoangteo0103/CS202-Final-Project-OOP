f9975cb6cc12daa85ca8f3512d2ba8d1420d59f1
hoangteo0103 <51512556+hoangteo0103@users.noreply.github.com>
2023-01-04 22:11:10 +0700

    TESTING

--- Lane.cpp
+++ Lane.cpp
@@ -67,7 +67,7 @@ Lane::Lane(int typeObstacle, int dir, int num, float speed, int type, Vector2f p
 
 	if (dir!=1)
 	{
-		Vector2f nowPos = {0.f - (num - 1) * (sizeTexture[typeObstacle] + disBetweenObstacle)  , pos.y };
+		Vector2f nowPos = {0.f - (num - 2) * (sizeTexture[typeObstacle] + disBetweenObstacle)  , pos.y };
 		for (int i = 1; i <= num; i++)
 		{
 			COBSTACLE* now = new COBSTACLE(this->obstacle_path, imageContainer, 0.1f, speed, nowPos, dir);
@@ -77,7 +77,7 @@ Lane::Lane(int typeObstacle, int dir, int num, float speed, int type, Vector2f p
 		reverse(obstacle.begin(), obstacle.end());
 	}
 	else {
-		Vector2f nowPos = { 2880.f +  (num - 1) * (sizeTexture[typeObstacle] + disBetweenObstacle)  , pos.y };
+		Vector2f nowPos = { 2880.f +  (num - 2) * (sizeTexture[typeObstacle] + disBetweenObstacle)  , pos.y };
 		for (int i = 1; i <= num; i++)
 		{
 			COBSTACLE* now = new COBSTACLE(this->obstacle_path, imageContainer, 0.1f, speed, nowPos, dir);
