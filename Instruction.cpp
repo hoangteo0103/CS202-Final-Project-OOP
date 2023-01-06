9aedcfa2d37df75414e6f9aca9f34feb3337f6b7
Cohvtk <94277200+Cohvtk@users.noreply.github.com>
2023-01-07 01:18:07 +0700

    Trim player texture

--- Instruction.cpp
+++ Instruction.cpp
@@ -86,7 +86,7 @@ void Instruction::updateInstructions()
         this->background.setTexture(texture);
         break;
     case 3:
-        texture.loadFromFile("External/images/author.png");
+        texture.loadFromFile("External/images/mainmenu3.png");
         this->background.setTexture(texture);
         break;
     case 4:
@@ -100,7 +100,7 @@ void Instruction::updateInstructions()
     case 6:
         this->buttons["NEXT_BTN"]->setPosition(Vector2f(app->getSize().x / 2 - 200.0, app->getSize().y));
         this->buttons["PREVIOUS_BTN"]->setPosition(Vector2f(app->getSize().x / 2 - 200.0, 555));
-        texture.loadFromFile("External/images/author.png");
+        texture.loadFromFile("External/images/mainmenu3.png");
         this->background.setTexture(texture);
         break;
     default:
